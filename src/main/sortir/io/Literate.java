5cd704eb7dbfd3e0afd274e2a219ad69dea8e3f7
axmszr <e0727266@u.nus.edu>
2023-08-06 21:06:17 +0800

    Add functionality

--- src/main/sortir/io/Literate.java
+++ src/main/sortir/io/Literate.java
@@ -1,40 +1,162 @@
 package sortir.io;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
+import sortir.exc.BadFileException;
 import sortir.exc.BadInputException;
 import sortir.exc.RageQuitException;
 
 public class Literate {
+    private static final String TXT = ".txt";
+
     private final Reader rd;
 
     public Literate() {
         this.rd = new Reader();
     }
 
-    private int choose(Consumer<Integer> sayer, int upperBound) throws RageQuitException {
+    private String get() {
+        return this.rd.read();
+    }
+
+    private int getChoice(Consumer<Integer> sayer, int upperBound) throws RageQuitException {
         for (int i = 0; i < 4; i++) {
             sayer.accept(i);
 
             try {
                 return this.rd.readInt(1, upperBound);
             } catch (BadInputException e) {
-                continue;
+                // continue
             }
         }
 
         throw new RageQuitException();
     }
 
+    private int getChoice(Consumer<Integer> sayer) throws RageQuitException {
+        return getChoice(sayer, Integer.MAX_VALUE);
+    }
+
     public int getActionChoice() throws RageQuitException {
-        return choose(Writer::sayActionChoices, 2);
+        return getChoice(Writer::sayActionChoices, 2);
     }
 
     public int getInputChoice() throws RageQuitException {
-        return choose(Writer::sayInputChoices, 2);
+        return getChoice(Writer::sayInputChoices, 2);
     }
 
     public int getRankChoice(String firstName, String secondName) throws RageQuitException {
-        return choose(i -> Writer.sayRankChoices(firstName, secondName, i), 3);
+        return getChoice(i -> Writer.sayRankChoices(firstName, secondName, i), 3);
+    }
+
+    public boolean confirm(List<String> list) throws RageQuitException {
+        int value = getChoice(i -> Writer.sayConfirmChoices(list, i), 2);
+        return (value == 1);
+    }
+
+    public int getInt() throws RageQuitException {
+        return getChoice(Writer::sayGetInt);
+    }
+
+    public List<String> manualRankerInput() throws RageQuitException {
+        List<String> inputs = new ArrayList<>();
+
+        int count = getInt();
+        for (int i = 0; i < count; i++) {
+            String input = get();
+            inputs.add(input);
+        }
+
+        Writer.saySuccess();
+        return inputs;
+    }
+
+    public List<String> manualMergerInput() throws RageQuitException {
+        List<String> inputs = new ArrayList<>();
+
+        int count = getInt();
+
+        Writer.sayMergerInputFormat();
+
+        for (int i = 0; i < count; i++) {
+            String input = get();
+            inputs.add(input);
+        }
+
+        return inputs;
+    }
+
+    public List<String> readFromFile() throws RageQuitException {
+        for (int i = 0; i < 4; i++) {
+            Writer.sayGetFilePath(i);
+            String filePath = get();
+
+            if (!filePath.endsWith(TXT)) {
+                filePath += TXT;
+            }
+
+            FileStuff file = new FileStuff(filePath);
+
+            try {
+                List<String> fileContent = file.loadFile();
+
+                if (fileContent.size() < 1) {
+                    throw new BadFileException("File is empty.");
+                }
+
+                Writer.saySuccess();
+                return fileContent;
+            } catch (FileNotFoundException e) {
+                // continue
+            } catch (BadFileException e) {
+                Writer.sayFileEmpty();
+                // continue
+            }
+        }
+
+        throw new RageQuitException();
+    }
+
+    public void saveToFile(String fileContent) throws RageQuitException {
+        for (int i = 0; i < 4; i++) {
+            Writer.sayGetSaveFilePath(i);
+            String filePath = get();
+
+            if (!filePath.endsWith(TXT)) {
+                filePath += TXT;
+            }
+
+            FileStuff file = new FileStuff(filePath + TXT);
+
+            try {
+                file.saveFile(fileContent);
+                Writer.saySuccess();
+            } catch (BadFileException e) {
+                // continue
+            }
+        }
+
+        throw new RageQuitException();
+    }
+
+    public boolean getSaveChoice() throws RageQuitException {
+        int value = getChoice(Writer::saySaveChoices, 2);
+        return (value == 1);
+    }
+
+    public boolean getRestartChoice() throws RageQuitException {
+        int value = getChoice(Writer::sayRestartChoices, 2);
+        return (value == 1);
+    }
+
+    public void bye() {
+        Writer.sayBye();
+    }
+
+    public void sayFailedMerger(String errorMessage) {
+        Writer.sayError("Invalid ranked list. Try again!", List.of(errorMessage));
     }
 }
--- src/main/sortir/io/Writer.java
+++ src/main/sortir/io/Writer.java
@@ -1,15 +1,22 @@
 package sortir.io;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Writer {
     private static final String HAPPY_FACE = "(^-^)";
     private static final String ANGRY_FACE = "(-_-)";
+    private static final String ERROR_FACE = "(x.x)";
     private static final String GAP = "     ";
     private static final List<String> noBody = List.of();
+    private static final String HOW_EVEN = "how even...";
     private static final String RAGE_QUIT = "...";
+    private static final String SUCCESS = "Done!";
     private static final String OI = "oi one more time ah";
     private static final String TIE = "[3] ~-TIE-~";
+    private static final String FILE_EMPTY = "That file is empty :/";
+    private static final String BYE = "Bye!";
+    private static final String MERGER_INPUT_HEADER = "Ranked List Format:";
     
     private static final List<String> ACTION_QUERY_HEADERS = List.of(
             "Hello! How can I help you today?",
@@ -36,6 +43,64 @@ public class Writer {
             "Type '1' or '2' to select, or '3' to call a tie!",
             "Just type '1', '2' or '3'.",
             OI);
+
+    private static final List<String> INT_QUERY_HEADERS = List.of(
+            "How many would you like to input?",
+            "Please provide an integer!",
+            "Just type an integer",
+            OI);
+
+    private static final List<String> CONFIRM_QUERY_HEADERS = List.of(
+            "Is this list correct?",
+            "Type '1' to confirm, or '2' to try again!",
+            "It's just '1' or '2'.",
+            OI);
+
+    private static final List<String> CONFIRM_QUERY_LEGS = List.of(
+            "",
+            "~-~-~",
+            "",
+            "[1] Confirm",
+            "[2] Retry");
+
+    private static final List<String> FILE_QUERY_HEADERS = List.of(
+            "Okay! What's the file path?",
+            "Please ensure the file is in the right folder!",
+            "Does this file exist.",
+            OI);
+
+    private static final List<String> SAVE_QUERY_HEADERS = List.of(
+            "Would you like to save this list?",
+            "Type '1' to save, or '2' to skip!",
+            "Just type '1' or '2'.",
+            OI);
+
+    private static final List<String> SAVE_QUERY_BODY = List.of(
+            "[1] Save",
+            "[2] Skip");
+
+    private static final List<String> SAVE_FILE_QUERY_HEADERS = List.of(
+            "Sure! What would you like to name the TXT file?",
+            "Please use a unique TXT file name!",
+            "That name is being used.",
+            OI);
+
+    private static final List<String> RESTART_QUERY_HEADERS = List.of(
+            "Would you like to do something else?",
+            "Type '1' to rank/merge again, or '2' to end the session!",
+            "Just type '1' or '2'.",
+            OI);
+
+    private static final List<String> RESTART_QUERY_BODY = List.of(
+            "[1] Continue",
+            "[2] Close");
+
+    private static final List<String> MERGER_INPUT_BODY = List.of(
+            "- Please use the format: <rank> #; <item name>",
+            "  e.g. 1 #; evermore",
+            "",
+            "- Please keep the items in ranked order, starting from 1.",
+            "  The formatting check comes after all items are given!");
 
     private static String repeat(String str, int times) {
         StringBuilder sb = new StringBuilder();
@@ -93,16 +158,12 @@ public class Writer {
         say(HAPPY_FACE, header, body);
     }
     
-    public static void sayHappy(String header) {
-        say(HAPPY_FACE, header, noBody);
-    }
-    
     public static void sayAngry(String header, List<String> body) {
         say(ANGRY_FACE, header, body);
     }
-    
-    public static void sayAngry(String header) {
-        say(ANGRY_FACE, header, noBody);
+
+    public static void sayError(String header, List<String> body) {
+        say(ERROR_FACE, header, body);
     }
     
     private static void sayHappyIndex(List<String> headerList, int i, List<String> body) {
@@ -121,9 +182,17 @@ public class Writer {
     public static void sayInputChoices(int i) {
         sayHappyIndex(INPUT_QUERY_HEADERS, i, INPUT_QUERY_BODY);
     }
-    
+
+    public static void sayHowEven() {
+        sayAngry(HOW_EVEN, noBody);
+    }
+
     public static void sayRageQuit() {
-        sayAngry(RAGE_QUIT);
+        sayAngry(RAGE_QUIT, noBody);
+    }
+
+    public static void saySuccess() {
+        sayHappy(SUCCESS, noBody);
     }
 
     private static List<String> makeRankBody(String firstName, String secondName) {
@@ -135,5 +204,51 @@ public class Writer {
     public static void sayRankChoices(String firstName, String secondName, int i) {
         List<String> rankBody = makeRankBody(firstName, secondName);
         sayHappyIndex(RANK_QUERY_HEADERS, i, rankBody);
+    }
+
+    private static List<String> makeConfirmBody(List<String> list) {
+        List<String> pointList = list.stream()
+                .map(str -> "- " + str)
+                .collect(Collectors.toList());
+        pointList.addAll(CONFIRM_QUERY_LEGS);
+
+        return pointList;
+    }
+
+    public static void sayConfirmChoices(List<String> list, int i) {
+        List<String> body = makeConfirmBody(list);
+        sayHappyIndex(CONFIRM_QUERY_HEADERS, i, body);
+    }
+
+    public static void sayGetInt(int i) {
+        sayHappyIndex(INT_QUERY_HEADERS, i, noBody);
+    }
+
+    public static void sayGetFilePath(int i) {
+        sayHappyIndex(FILE_QUERY_HEADERS, i, noBody);
+    }
+
+    public static void sayFileEmpty() {
+        sayHappy(FILE_EMPTY, noBody);
+    }
+
+    public static void saySaveChoices(int i) {
+        sayHappyIndex(SAVE_QUERY_HEADERS, i, SAVE_QUERY_BODY);
+    }
+
+    public static void sayGetSaveFilePath(int i) {
+        sayHappyIndex(SAVE_FILE_QUERY_HEADERS, i, noBody);
+    }
+
+    public static void sayRestartChoices(int i) {
+        sayHappyIndex(RESTART_QUERY_HEADERS, i, RESTART_QUERY_BODY);
+    }
+
+    public static void sayBye() {
+        sayHappy(BYE, noBody);
+    }
+
+    public static void sayMergerInputFormat() {
+        sayHappy(MERGER_INPUT_HEADER, MERGER_INPUT_BODY);
     }
 }
